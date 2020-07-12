f6ed96e7a79c330f1874283f8ad4d1f880f43867
Caroline Tornquist <63213640+ctornquist@users.noreply.github.com>
2020-07-12 11:31:10 -0400

    did most of it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Here we go!');
+
+function myTimer() {
+  const d = new Date();
+  document.getElementById('main').innerHTML = `You've been on this page ${d.getSeconds()} seconds`;
+}
+
+setInterval(myTimer(), 1000);

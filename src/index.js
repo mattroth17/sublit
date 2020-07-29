b946e0b87e7a2afa1891ce7cd696d68dd3291510
Caroline Tornquist <63213640+ctornquist@users.noreply.github.com>
2020-07-29 12:33:19 -0400

    added redux

--- src/index.js
+++ src/index.js
@@ -1,50 +1,23 @@
-import './style.scss';
-import {
-  BrowserRouter as Router, Route, NavLink, Switch,
-} from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
-const About = (props) => {
-  return <div> All there is to know about me </div>;
-};
-const Welcome = (props) => {
-  return <div>Welcome</div>;
-};
-const Test = (props) => {
-  return <div> ID: {props.match.params.id} </div>;
-};
-const FallBack = (props) => {
-  return <div>URL Not Found</div>;
-};
+import reducers from './reducers';
 
-const App = (props) => {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/about" component={About} />
-          <Route exact path="/test/:id" component={Test} />
-          <Route component={FallBack} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+import App from './components/app';
 
-const Nav = (props) => {
-  return (
-    <nav>
-      <ul>
-        <li><NavLink to="/" exact>Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
-      </ul>
-    </nav>
-  );
-};
+// this creates the store with the reducers, and does some other stuff to initialize devtools
+// boilerplate to copy, don't have to know
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+));
 
-ReactDOM.render(<App />, document.getElementById('main'));
+// we now wrap App in a Provider
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('main'),
+);

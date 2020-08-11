9ed5d52a404478779fadde3f12a3f681db64199d
Sada R. Nichols-Worley <sada.r.nichols-worley.22@dartmouth.edu>
2020-08-11 18:34:14 -0400

    Adding chat feature

---
+++ src/components/chat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Chat = (props) => {
+    return (
+      <div>
+        
+      </div>
+    );
+  };
+  
+  export default connect(null, { })(Chat);
+
+  

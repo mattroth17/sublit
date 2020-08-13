49eab1b5ecbcb261799c4fa2d0e477bb596a928d
matt roth <matthew.c.roth.22@dartmouth.edu>
2020-08-13 12:42:53 -0500

    add reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
 
+import ListingReducer from './posts_reducer';
+import ErrorReducer from './error_reducer';
+import AuthReducer from './auth_reducer';
+
 const rootReducer = combineReducers({
-  
+  listings: ListingReducer,
+  errors: ErrorReducer,
+  auth: AuthReducer,
 });
 
 export default rootReducer;
--- src/reducers/listing_reducer.js
+++ src/reducers/listing_reducer.js
@@ -2,18 +2,22 @@ import { ActionTypes } from '../actions';
 
 //define initial state
 const init = {
-
+  all = [],
+  current = {},
 };
-
 
 /**
  * Manages the local posts on your app
  */
-const listingReducer = (state = init, action) => {
+const ListingReducer = (state = init, action) => {
   switch (action.type) {
+    case ActionTypes.FETCH_LISTINGS:
+      return { all: action.payload, current: {} };
+    case ActionTypes.FETCH_LISTING:
+      return { ...state, current: action.payload };
     default:
       return state;
   }
 };
 
-export default listingReducer;
+export default ListingReducer;
